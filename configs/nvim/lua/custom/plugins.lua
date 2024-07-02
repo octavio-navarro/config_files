74fa2d2e58d47476c3a8ae088c9c7a0a9f142768
Octavio Navarro <octavio.navarro@tec.mx>
2024-07-01 20:08:02 -0600

    Fix nvim config.

---
+++ configs/nvim/lua/custom/plugins.lua
@@ -0,0 +1,35 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "mypy",
+        "ruff",
+        "pyright",
+        "typescript-language-server"
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "pocco81/auto-save.nvim",
+    enabled = true,
+    lazy = false
+  }
+}
+
+return plugins
